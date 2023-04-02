2f20686b79b159df30d367d79901b45b43951682
kimaychan <mikeebersabal@gmail.com>
2023-04-02 15:31:53 +0800

    setup express and listen to port 9000

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log('hello world')
+import express from 'express'
+import routes from './routes'
+import appconf from './../appconf'
+
+const app = express()
+app.use(routes)
+
+app.listen(appconf.PORT, () => {
+  console.log('app is listening to PORT 9000')
+})
